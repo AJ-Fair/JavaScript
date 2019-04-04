aaf63f077ec83592b8fe40ac81194b45d7c96bf6
AJ-Fair <48727141+AJ-Fair@users.noreply.github.com>
2019-04-04 11:49:24 -0400

    Add files via upload

---
+++ Dog Age.js
@@ -0,0 +1,14 @@
+// This variable is my age. //
+const myAge = 29;
+let earlyYears = 2;
+earlyYears *= 10.5;
+// This variable is calculated by subtracting two from myAge. //
+let laterYears = myAge - 2;
+// This line multiplies laterYears by four and reassigns it a new value. //
+laterYears *= 4;
+// This line calculates my age in dog years. //
+let myAgeInDogYears = earlyYears + laterYears;
+// This line is my name. //
+const myName = 'AJ Fair'.toLowerCase();
+// This line prints out the results of my code. //
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
